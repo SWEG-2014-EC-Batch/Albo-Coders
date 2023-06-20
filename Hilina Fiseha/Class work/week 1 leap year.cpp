8d2f9ada0f108e7747eb9bf07a9176bb6177d07c
Hilina19 <136348633+Hilina19@users.noreply.github.com>
2023-06-20 20:25:36 +0300

    Create week 1 leap year.cpp

---
+++ Hilina Fiseha/Class work/week 1 leap year.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+bool Year(int x);
+
+int main()
+{
+    int x;
+    cout << Year(x);
+    if (Year(x) == 1)
+    {
+        cout << "It's a leap year";
+    }
+    return 0;
+}
+
+bool Year(int x)
+{
+    cout << "Enter a year for checking wheather or not it's a leap year" << endl;
+    cin >> x;
+    if (x % 4 == 0 ^ (x % 100 != 0 && x % 400 == 0))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
