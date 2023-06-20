98478614f8fc2a5fcc0db71cd2c6cdb913e77131
Hewialex <123300353+Hewialex@users.noreply.github.com>
2023-06-20 23:48:14 +0300

    Create Header 1.h

---
+++ Hewan Alemayehu/Home work 2/Class works/Week 2/Header 1.h
@@ -0,0 +1,12 @@
+#if n define new ques.h
+#define new ques.h
+
+#include <iostream>
+#include"new ques.h"
+using namespace std;
+
+double getProduct(int num1 , double num2){
+    double product = num1 * num2;
+    return product;
+}
+#endif
