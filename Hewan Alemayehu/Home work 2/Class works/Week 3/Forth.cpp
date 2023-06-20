860416a485637f9e1210dc14aa1ca58e649da376
Hewialex <123300353+Hewialex@users.noreply.github.com>
2023-06-20 14:31:22 +0300

    Create Forth.cpp

---
+++ Hewan Alemayehu/Home work 2/Class works/Week 3/Forth.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * factorial(n-1);
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter a non-negative integer: ";
+    cin >> n;
+    cout << "Factorial of " << n << ": " << factorial(n) << endl;
+    return 0;
+}
