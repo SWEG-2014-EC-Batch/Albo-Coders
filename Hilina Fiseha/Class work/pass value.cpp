88df76fc63b85c8179367ce5b891128a864eec55
Hilina19 <136348633+Hilina19@users.noreply.github.com>
2023-06-20 20:40:27 +0300

    Create pass value.cpp

---
+++ Hilina Fiseha/Class work/pass value.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+// function prototype
+void calcQuotient(double n1, double n2, double* answer);
+
+int main() {
+    double num1, num2, quotient;
+
+    // read in values for num1 and num2
+    std::cout << "Enter two numbers: ";
+    std::cin >> num1 >> num2;
+
+    // call calcQuotient function
+    calcQuotient(num1, num2, &quotient);
+
+    // print quotient
+    std::cout << "The quotient is " << quotient << std::endl;
+
+    return 0;
+}
+
+// function definition
+void calcQuotient(double n1, double n2, double* answer) {
+    *answer = n1 / n2;
+}
