6e415286ede34dc1fee6c7640532908a98fc8fa4
Hewialex <123300353+Hewialex@users.noreply.github.com>
2023-06-20 20:50:19 +0300

    Update First.cpp

--- Hewan Alemayehu/Home work 2/Class works/Week 2/First.cpp
+++ Hewan Alemayehu/Home work 2/Class works/Week 2/First.cpp
@@ -1,1 +1,17 @@
-//
+#include <iostream>
+using namespace std;
+
+void mult(float num1, float num2) {
+    float result = num1 * num2;
+    cout << result << endl;
+}
+
+int main() {
+    float num1, num2;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    mult(num1, num2);
+    return 0;
+}
