e7af3bf0ae2c55e31ad51b5bbe30f3166a22c740
Hewialex <123300353+Hewialex@users.noreply.github.com>
2023-06-20 20:47:12 +0300

    Create Question 12 ref.cpp

---
+++ Hewan Alemayehu/Home work 2/Class works/Week 2/Question 12 ref.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+Using namespace std;
+
+void calcQuotient(double n1, double n2, double* answer);
+
+int main() {
+    double num1, num2, quotient;
+
+    
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    
+    calcQuotient(num1, num2, &quotient)
+
+    
+    cout << "The quotient is " << quotient << endl;
+
+    return 0;
+}
+
+
+void calcQuotient(double n1, double n2, double* answer) {
+    *answer = n1 / n2;
+}
