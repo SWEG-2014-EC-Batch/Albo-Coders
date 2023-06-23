2d35198ba4c8302b34a84289999745da685d8147
Hilina19 <136348633+Hilina19@users.noreply.github.com>
2023-06-24 00:30:17 +0300

    Create sum of digits of a number .cpp

---
+++ Hilina Fiseha/Class work/week 3/sum of digits of a number .cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int sum_of_digits(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    else {
+        return (n % 10) + sum_of_digits(n / 10);
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    int sum = sum_of_digits(n);
+    cout << "Sum of digits of " << n << " is " << sum << endl;
+    return 0;
+}
