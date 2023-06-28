06373048199fb5b767207b3822c97f64098f25bd
Hermela Ejigu <125656902+Hermii1@users.noreply.github.com>
2023-06-28 21:09:41 +0300

    Create struct phone A.cpp
    version 1

---
+++ Hermela Ejigu/Class Work/Week 4/struct phone A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+struct Phone {
+    int area_code;
+    int exchange;
+    int number;
+};
+
+int main() {
+    
+    Phone my_phone = {212, 767, 8900};
+
+   
+    Phone user_phone;
+    cout << "Enter phone number (area code, exchange, number): ";
+    cin >> user_phone.area_code >> user_phone.exchange >> user_phone.number;
+
+    
+    cout << "My phone number: (" << my_phone.area_code << ") " << my_phone.exchange << "-" << my_phone.number << endl;
+    cout << "User phone number: (" << user_phone.area_code << ") " << user_phone.exchange << "-" << user_phone.number << endl;
+
+    return 0;
+}
