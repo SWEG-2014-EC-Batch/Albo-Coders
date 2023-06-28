ff9c64c4beb69c1c4644512f78a2f5a5507d04a5
Hermela Ejigu <125656902+Hermii1@users.noreply.github.com>
2023-06-28 21:12:56 +0300

    Create struct phone B.cpp
    version 1

---
+++ Hermela Ejigu/Class Work/Week 4/struct phone B.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+struct phone {
+    double areaCode;
+    double exchange;
+    double number;
+};
+
+void displayPhoneNumber(const phone& p) {
+    cout << "(" << p.areaCode << ")" << p.exchange << "-" << p.number << endl;
+}
+
+int main() {
+    int numPhoneNumbers;
+
+    cout << "Enter the number of phone numbers: ";
+    cin >> numPhoneNumbers;
+
+       phone phoneNumbers[numPhoneNumbers];
+
+     cout << "Enter the phone numbers:" << endl;
+
+    for (int i = 0; i < numPhoneNumbers; ++i) {
+        cout << "Phone Number " << (i + 1) << ":" << endl;
+
+        cout << "Area Code: ";
+        cin >> phoneNumbers[i].areaCode;
+
+        cout << "Exchange: ";
+        cin >> phoneNumbers[i].exchange;
+
+        cout << "Number: ";
+        cin >> phoneNumbers[i].number;
+
+        cout << endl;
+    }
+
+    cout << endl;
+    cout << "Phone Numbers:" << endl;
+    cout << "Phone Number 1: ";
+
+    for (int i = 0; i < numPhoneNumbers; ++i) {
+        cout << "Phone Number " << (i + 2) << ": ";
+        displayPhoneNumber(phoneNumbers[i]);
+    }
+
+    cout << endl;
+
+    return 0;
+}
