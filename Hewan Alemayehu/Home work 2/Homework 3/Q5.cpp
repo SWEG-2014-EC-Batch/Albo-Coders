c849be947a42b679ef056689fdab95e898359d55
Hewialex <123300353+Hewialex@users.noreply.github.com>
2023-07-11 23:03:21 +0300

    Create Q5.cpp

---
+++ Hewan Alemayehu/Home work 2/Homework 3/Q5.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+
+int main() {
+
+   ifstream input_file("data.txt");
+    if (!input_file.is_open()) {
+        cerr << "Failed to open input file\n";
+       return 1;
+    }
+
+
+    int num_groups, num, count;
+    double sum, average;
+    while (input_file >> num_groups) {
+        sum = 0.0;
+        count = 0;
+        for (int i = 0; i < num_groups; i++) {
+            input_file >> num;
+            sum += num;
+            count++;
+        }
+        average = sum / count;
+        cout << "Average of group of " << num_groups << " numbers: " << average <<endl;
+    }
+
+
+    input_file.close();
+
+    return 0;
+}
