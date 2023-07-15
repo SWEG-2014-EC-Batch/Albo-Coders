3c95b8d17575d2bcdebcee414c9a40711e00f858
Hilina19 <136348633+Hilina19@users.noreply.github.com>
2023-07-15 14:51:24 +0300

    Update rain fall ver 2 .cpp

--- project- code refactoring/rain fall ver 2 .cpp
+++ project- code refactoring/rain fall ver 2 .cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+using namespace std;
 
+const int NUM_MONTHS = 12;
+const string MONTHS[NUM_MONTHS] = {
+    "Meskerem", "Tikimt", "Hidar", "Tahsas", "Tir", "Yekatit",
+    "Megabit", "Miazia", "Genbot", "Sene", "Hamle", "Nehase"
+};
+
+struct RainfallData {
+    double averageRainfall;
+    double monthlyRainfall[NUM_MONTHS];
+};
+
+void displayRainfallTable(const RainfallData& data)
+{
+    cout << "Month\t\tRainfall\tDeviation from Average\n";
+    cout << "--------------------------------------------\n";
+
+    for (int i = 0; i < NUM_MONTHS; ++i)
+    {
+        double rainfall = data.monthlyRainfall[i];
+        double deviation = rainfall - data.averageRainfall;
+
+        cout << left << setw(10) << MONTHS[i];
+        cout << right << setw(10) << rainfall;
+        cout << right << setw(10) << deviation << endl;
+    }
+}
