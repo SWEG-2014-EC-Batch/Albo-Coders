b60cfc9a241d143f882c55534b231c97421a609c
Hewialex <123300353+Hewialex@users.noreply.github.com>
2023-07-15 14:55:50 +0300

    Update leap year with function.cpp

--- project- code refactoring/leap year with function.cpp
+++ project- code refactoring/leap year with function.cpp
@@ -11,3 +11,20 @@ bool isLeapYear(int year) {
     }
     return false;
 }
+
+bool isValidDate(int day, int month, int year) {
+    if (month < 1  month > 12) {
+        return false;
+    }
+
+    int daysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    if (isLeapYear(year)) {
+        daysInMonth[1] = 29;
+    }
+
+    if (day < 1  day > daysInMonth[month - 1]) {
+        return false;
+    }
+
+    return true;
+}
