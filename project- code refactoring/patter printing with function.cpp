0c81a4266251e0f76b0ae9766b03228615d704f8
Hilina19 <136348633+Hilina19@users.noreply.github.com>
2023-07-15 14:44:58 +0300

    Update patter printing with function.cpp

--- project- code refactoring/patter printing with function.cpp
+++ project- code refactoring/patter printing with function.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+
+using namespace std;
+// pattern print using function
+void printPattern(char letter, char limit, int space)
+{
+    for (char i = letter; i >= limit; i--)
+    {
+        for (char j = limit; j <= i; j++)
+        {
+            cout << j;
+        }
+        for (int k = 0; k < space; k++)
+        {
+            cout << " ";
+        }
+        for (char j = i; j >= limit; j--)
+        {
+            if (j < letter)
+                cout << j;
+        }
+        space += 2;
+        cout << endl;
+    }
 
